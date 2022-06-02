c79809e5a484911da7fcbbf27959f14c02ac0d8e
LucianoBrumer <lucianobrumer5@gmail.com>
2022-06-02 16:21:08 -0300

    speed spcket

--- public/main.js
+++ public/main.js
@@ -207,6 +207,10 @@ class Starship extends TruonObject{
                 id: this.id,
                 x: this.x,
                 y: this.y,
+                speedLeft: this.speedLeft,
+                speedRighty: this.speedRight,
+                speedUp: this.speedUp,
+                speedDown: this.speedDown,
                 facing: this.facing,
             })
 
@@ -319,8 +323,10 @@ socket.on('players', socketPlayers => {
                 players.forEach(editPlayer => {
                     if(editPlayer.id === socketPlayer.id){
                         if(document.body.contains(editPlayer.element)){
-                            editPlayer.x = socketPlayer.x;
-                            editPlayer.y = socketPlayer.y;
+                            editPlayer.speedLeft = socketPlayer.speedLeft;
+                            editPlayer.speedRight = socketPlayer.speedRight;
+                            editPlayer.speedUp = socketPlayer.speedUp;
+                            editPlayer.speedDown = socketPlayer.speedDown;
                             editPlayer.facing = socketPlayer.facing;
                         }else{
                             players = players.filter(x => x.id !== socketPlayer.id);
