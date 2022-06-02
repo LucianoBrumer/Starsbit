47eb71801d0968168a827ddf071b4bfaf315fcff
LucianoBrumer <lucianobrumer5@gmail.com>
2022-06-02 14:32:57 -0300

    index

--- public/main.js
+++ public/main.js
@@ -364,7 +364,7 @@ const scoreBoard = document.getElementById('scoreboard')
 socket.on('kills', kills => {
 
     Object.entries(kills).forEach(([key, value], index) => {
-        if(index < 10) scoreBoard.children[i].textContent = `${i+1}# ${key}: ${value}`
+        if(index < 10) scoreBoard.children[index].textContent = `${i+1}# ${key}: ${value}`
     })
 
 })
