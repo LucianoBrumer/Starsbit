05896e6f7990ce347eb260a1fac02d295d92b6b6
LucianoBrumer <lucianobrumer5@gmail.com>
2023-10-07 21:46:20 -0300

    test4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config()
+require('dotenv').config()
 
 const path = require('path')
 const cors = require('cors')
@@ -18,7 +18,7 @@ const app = express()
 // }
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.json({
@@ -35,66 +35,50 @@ const server = app.listen(app.get('port'), require('dns').lookup(require('os').h
 const SocketIO = require('socket.io')
 const io = SocketIO(server)
 
-// const kills = {}
-// const connections = {}
+const kills = {}
+const connections = {}
 
-// io.on('connection', socket => {
-//     console.log("[SOCKET.IO] New Connection:", socket.id)
+io.on('connection', socket => {
+    console.log("[SOCKET.IO] New Connection:", socket.id)
 
-//     socket.on('player', player => {
-//         try {
-//             connections[player.id] = socket.id
-//             io.sockets.emit("player", player);
-//         } catch (error) {
-//             console.error(`[SOCKET.IO:ON'player' ERROR], ${error}}`);
-//         }
-//     })
+    socket.on('player', player => {
+        try {
+            connections[player.id] = socket.id
+            io.sockets.emit("player", player);
+        } catch (error) {
+            console.error(`[SOCKET.IO:ON'player' ERROR], ${error}}`);
+        }
+    })
 
-//     socket.on('shot', playerId => {
-//         try {
-//             io.sockets.emit('shot', playerId)
-//         } catch (error) {
-//             console.error(`[SOCKET.IO:ON'shot' ERROR], ${error}}`);
-//         }
-//     })
+    socket.on('shot', playerId => {
+        try {
+            io.sockets.emit('shot', playerId)
+        } catch (error) {
+            console.error(`[SOCKET.IO:ON'shot' ERROR], ${error}}`);
+        }
+    })
 
-//     socket.on('death', playerKillerId => {
-//         try {
-//             kills[playerKillerId] = kills[playerKillerId] === undefined ? 0 : kills[playerKillerId]+1
-//             io.sockets.emit('kills', kills)
-//         } catch (error) {
-//             console.error(`[SOCKET.IO:ON'death' ERROR], ${error}}`);
-//         }
-//     })
+    socket.on('death', playerKillerId => {
+        try {
+            kills[playerKillerId] = kills[playerKillerId] === undefined ? 0 : kills[playerKillerId]+1
+            io.sockets.emit('kills', kills)
+        } catch (error) {
+            console.error(`[SOCKET.IO:ON'death' ERROR], ${error}}`);
+        }
+    })
 
-//     socket.on("disconnect", () => {
-//         try {
-//             Object.entries(connections).forEach(([playerId, socketId]) => {
-//                 if(socket.id === socketId){
-//                     delete connections[playerId]
-//                     delete kills[playerId]
-//                     io.sockets.emit('displayer', playerId)
-//                 }
-//             })
-//         } catch (error) {
-//             console.error(`[SOCKET.IO:ON'disconnect' ERROR], ${error}}`);
-//         }
-//     })
+    socket.on("disconnect", () => {
+        try {
+            Object.entries(connections).forEach(([playerId, socketId]) => {
+                if(socket.id === socketId){
+                    delete connections[playerId]
+                    delete kills[playerId]
+                    io.sockets.emit('displayer', playerId)
+                }
+            })
+        } catch (error) {
+            console.error(`[SOCKET.IO:ON'disconnect' ERROR], ${error}}`);
+        }
+    })
 
-// })
-
-// const express = require('express')
-// const cors = require('cors')
-
-// const app = express()
-
-// app.use(cors())
-// app.get('/', (req, res) => {
-//     res.json({
-//         "hola": "mundo"
-//     })
-// })
-
-// app.listen(3000, () => {
-//     console.log(`Working`)
-// })
+})
