2571807937fa9cadf75665fa5aaad18fb5cd1f51
Luciano Brumer <lucianobrumer@gmail.com>
2024-03-31 19:43:00 -0300

    using pyxes

--- src/assets/GameObjects/World.js
+++
@@ -1,9 +0,0 @@
-export default {
-    color: 'rgb(15, 15, 35)',
-    load: current => {
-        current.x = -current.scene.worldLimit
-        current.y = -current.scene.worldLimit
-        current.width = current.scene.worldLimit * 2
-        current.height = current.scene.worldLimit * 2
-    }
-}
